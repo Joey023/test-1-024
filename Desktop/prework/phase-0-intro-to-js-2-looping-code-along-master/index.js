aa358ab2878e008233ef639986e72fc5f72a4c0b
Joseph Santos <josephsantos024@gmail.com>
2021-12-07 04:16:55 -0600

    Completed assignment

---
+++ Desktop/prework/phase-0-intro-to-js-2-looping-code-along-master/index.js
@@ -0,0 +1,22 @@
+// Code your solutions in this fil
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards () {
+    return [
+        "Thank you, Guadalupe, for the wonderful surprise gift!"
+        ,  "Thank you, Ollie, for the wonderful surprise gift!"
+         , "Thank you, Aki, for the wonderful surprise gift!",]};
+
+         
+         
+
+           
+
+         
+
+         function countDown(number){
+            while (number >= 0){
+            console.log(number--)
+            }
+        }
